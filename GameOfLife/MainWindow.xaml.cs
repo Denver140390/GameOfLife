ce87aba18441b6c559e161705e6d5eb1b17614d1
Denver140390 <Freeman140390@gmail.com>
2015-11-16 18:01:23 +0300

    Wrote methods to parse byte[] to string[] and cleaned up the code a bit.

--- GameOfLife/MainWindow.xaml.cs
+++ GameOfLife/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq;
 using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -16,34 +19,86 @@ namespace GameOfLife
         private WriteableBitmap _bitmap;
         private Random _random = new Random();
 
+        private WebServiceController wsc = new WebServiceController();
+
+        private const int CELLS_IN_ROW = 10;
+        private const int CELLS_IN_COLUMN = 10;
+        private const int CELLS_AMOUNT = CELLS_IN_COLUMN * CELLS_IN_ROW;
+        private Int32Rect sourceRect = new Int32Rect(0, 0, CELLS_IN_ROW, CELLS_IN_COLUMN);
+        private int _sourceBufferStride = 0;
+
         public MainWindow()
         {
             InitializeComponent();
 
             DataContext = this;
 
-            Bitmap = new WriteableBitmap(10, 10, 96, 96, PixelFormats.Gray8, BitmapPalettes.BlackAndWhite);
+            Bitmap = new WriteableBitmap(CELLS_IN_ROW, CELLS_IN_COLUMN, 96, 96, PixelFormats.Gray8, BitmapPalettes.BlackAndWhite);
             RenderOptions.SetBitmapScalingMode(Image, BitmapScalingMode.NearestNeighbor);
+            _sourceBufferStride = CELLS_IN_ROW * Bitmap.Format.BitsPerPixel / 8;
 
-            CalculateFirstGeneration();
+            RunGame();
         }
 
-        public void CalculateFirstGeneration()
+        private async void RunGame()
         {
-            var bytes = new byte[100];
-            for (int i = 0; i < 100; i++)
+            //Generate first generation
+            var bytes = new byte[CELLS_AMOUNT];
+            for (int i = 0; i < CELLS_AMOUNT; i++)
             {
-                bytes[i] = (byte)(_random.Next(0, 256) / 128 * 255);
+                bytes[i] = (byte)(_random.Next(0, 2) * 255);
             }
             FillBitmap(bytes);
+
+            //Run the game
+            while (true)
+            {
+                bytes = RequestNextGeneration(bytes);
+                FillBitmap(bytes);
+                await Task.Delay(10);
+            }
         }
 
-        public void FillBitmap(byte[] sourceBytes)
+        private byte[] RequestNextGeneration(byte[] bytes)
         {
-            Int32Rect sourceRect = new Int32Rect(0, 0, 10, 10);
-            int sourceBufferStride = 10 * Bitmap.Format.BitsPerPixel / 8;
+            //Transform byte[] into string[] and add row number in the beginning of each string
+            var cellStrings = new string[CELLS_IN_COLUMN]; //Row example: 50011001100
+            for (int i = 0; i < CELLS_IN_COLUMN; i++)
+            {
+                cellStrings[i] = i.ToString();
+                for (int j = 0; j < CELLS_IN_ROW; j++)
+                {
+                    int index = j + i * CELLS_IN_ROW;
+                    cellStrings[i] += bytes[index] / 255;
+                }
+            }
 
-            Bitmap.WritePixels(sourceRect, sourceBytes, sourceBufferStride, 0);
+            //Request next generation
+            byte[,] nextGen = wsc.Request(cellStrings);
+
+            //Uncomment this to see a random behaviour without using web service
+//            byte[,] nextGen = new byte[CELLS_IN_ROW, CELLS_IN_COLUMN + 1];
+//            for (int i = 0; i < CELLS_IN_ROW; i++)
+//            {
+//                for (int j = 0; j < CELLS_IN_COLUMN + 1; j++)
+//                {
+//                    nextGen[i, j] = (byte)(_random.Next(0, 2)*255);
+//                }
+//            }
+
+            //Transform byte[,] into byte[] and remove first element from every row
+            List<byte> nextList = nextGen.Cast<byte>().ToList();
+            for (int i = 0; i < CELLS_IN_ROW; i++)
+            {
+                nextList.RemoveAt(i * CELLS_IN_ROW);
+            }
+
+            return nextList.Cast<byte>().ToArray();
+        }
+
+        private void FillBitmap(byte[] sourceBytes)
+        {
+            Bitmap.WritePixels(sourceRect, sourceBytes, _sourceBufferStride, 0);
         }
 
         public WriteableBitmap Bitmap
---
+++ GameOfLife/WebServiceController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace GameOfLife
+{
+    //TODO 
+    class WebServiceController
+    {
+        public byte[,] Request(string[] cells)
+        {
+            string url = "http://game-of-life-ws.herokuapp.com/next";
+            WebRequest request = WebRequest.Create(url);
+            request.Method = "POST";
+
+            ///TODO input parameter
+            string postData = "This is a test that posts this string to a Web server.";
+
+            byte[] byteArray = Encoding.UTF8.GetBytes(postData);
+            // Set the ContentType property of the WebRequest.
+            request.ContentType = "application/json";
+            // Set the ContentLength property of the WebRequest.
+            request.ContentLength = byteArray.Length;
+            // Get the request stream.
+            Stream dataStream = request.GetRequestStream();
+            // Write the data to the request stream.
+            dataStream.Write(byteArray, 0, byteArray.Length);
+
+            // Close the Stream object.
+            dataStream.Close();
+            // Get the response.
+            WebResponse response = request.GetResponse();
+            // Display the status.
+            Console.WriteLine(((HttpWebResponse)response).StatusDescription);
+            // Get the stream containing content returned by the server.
+            dataStream = response.GetResponseStream();
+            // Open the stream using a StreamReader for easy access.
+            StreamReader reader = new StreamReader(dataStream);
+            // Read the content.
+            string responseFromServer = reader.ReadToEnd();
+
+            byte[,] nextgen = JsonConvert.DeserializeObject<byte[,]>(responseFromServer);
+
+            // Clean up the streams.
+            reader.Close();
+            response.Close();
+
+            return nextgen;
+        }
+    }
+}
